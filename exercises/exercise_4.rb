7d5a2bb6a7b7cb9c3f137a7f6fbc40be815f6156
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-09 22:26:11 +0000

    Complete 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,21 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
+Surreystore = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel:false, womens_apparel: true)
+Whistlerstore = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel:true, womens_apparel: false)
+Yaletownstore = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel:true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+@womens_stores = Store.where(["mens_apparel = ? and womens_apparel = ? and annual_revenue < ?", false, true, 1000000])
+
+@mens_stores.each do |item|
+    puts "#{item.name} #{item.annual_revenue}"
+end
+
+@womens_stores.each do |item|
+    puts "#{item.name} #{item.annual_revenue}"
+end
+
+
+
 # Your code goes here ...
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+sum = Store.sum(:annual_revenue)
+
+puts sum 
+
+puts sum/Store.count
+
+puts Store.where(["annual_revenue > ?",1000000]).count
 # Your code goes here ...
